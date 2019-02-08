b5816a6ed3fa464390b770c2e576b133af573b72
Bruno Albuquerque Brito <bruno.brito@startmeup.com.br>
2019-02-08 15:42:26 -0200

    Utilizando CDI para usar os Beans do JSF

--- src/main/java/br/com/alura/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/alura/loja/beans/AdminLivrosBean.java
@@ -1,5 +1,22 @@
 package br.com.alura.loja.beans;
 
-public class AdminLivrosBean {
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
 
+import br.com.alura.loja.models.Livro;
+import lombok.Data;
+
+
+@Data
+@Named
+@RequestScoped
+public class AdminLivrosBean {
+	
+	
+	private Livro livro = new Livro();
+	
+	public void salvar() {
+		System.out.println("Livros cadastrado: "+ livro);
+	}
 }
